3fe10c73b2d81723325f3ad55f926228d5fb7f76
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-03-30 15:18:52 -0300

    requisito 3 plus linter

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
 
     return (
-      <h2> { headline } </h2>
+      <h2>
+        { headline }
+      </h2>
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
