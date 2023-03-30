817b0d271076fa3e5d2d6688cdf8a79e2b34b92f
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-03-30 14:42:10 -0300

    lint aplicado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <h1>Sistema Solar</h1>
+      <header>
+        <h1>Sistema Solar</h1>
+
+      </header>
     );
   }
 }
