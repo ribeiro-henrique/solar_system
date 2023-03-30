dcd01df807ba677fc3a23e8b5d09c08e6c902acf
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-03-30 16:44:01 -0300

    lint me complicou nesse

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard planetName={ name } planetImage={ image } key={ name } />))}
       </div>
     );
   }
