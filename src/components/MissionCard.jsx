379a4058b7e722f006dd3b34e71926ef8ecde7e9
ribeiro-henrique <hesr.ribeiro@gmail.com>
2023-03-30 17:52:49 -0300

    requisito 9 plus linter

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name } = this.props;
+    const { year } = this.props;
+    const { country } = this.props;
+    const { destination } = this.props;
+
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{ name }</p>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
+      </div>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
+
+export default MissionCard;
